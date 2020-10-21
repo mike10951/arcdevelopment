b89efe5991ee8497912e6021f0c26b74bcd54f3f
mike10951 <miguel.cabrera.1095@gmail.com>
2020-10-21 14:46:10 -0500

    Set theme and inline styling.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './ui/Theme';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button';
 import Header from './ui/Header';
 
 function App() {
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
       <h1>Hello world</h1>
       <Button variant='contained' color='primary'>
         Hello World
       </Button>
-    </React.Fragment>
+      <p>
+        {[...new Array(200)]
+          .map(
+            () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+          )
+          .join('\n')}
+      </p>
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    toolbarMargin: {
+        ...theme.mixins.toolbar
+    }
+}))
 
 const Header = (props) => {
-  return (
-    // Notice that position is set to "sticky". CssBaseline is necessary; otherwise, text would be hidden behind the AppBar component.
-    <AppBar position='sticky'>
-      <Toolbar>Arc Development</Toolbar>
-    </AppBar>
-  );
+    const classes = useStyles()
+
+    return (
+        <React.Fragment>
+            <ElevationScroll>
+                {/* Notice that position is set to "sticky". CssBaseline is necessary; otherwise, text would be hidden behind the AppBar component. */}
+                <AppBar position='sticky' color='primary'>
+                    <Toolbar>
+                        <Typography variant="h3">
+                            Arc Development
+                    </Typography>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            {/* This div would be necessary if CssBaseline was not used. */}
+            {/* <div className={classes.toolbarMargin}></div> */}
+        </React.Fragment>
+    );
 };
+
+function ElevationScroll(props) {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
 
 export default Header;
